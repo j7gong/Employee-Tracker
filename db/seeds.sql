78e4bca345ae8ee0454491fe01b708f831a5cff4
Jingsi <gongjingsi@hotmail.com>
2022-11-16 14:52:23 -0500

    set up employee table

--- db/seeds.sql
+++ db/seeds.sql
@@ -11,3 +11,11 @@ VALUES
     ('Salesperson', 8000, 3), 
     ('Legal Team Lead', 25000, 4), 
     ('Software Engineer', 15000, 2);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES 
+    ('Jane', 'Jan', 1, 1), 
+    ('James', 'Feb', 2, 1), 
+    ('Matt', 'Mar', 3, 1), 
+    ('Janny', 'May', 3, 2),
+    ('Jimmy', 'Jun', 4, 2);
