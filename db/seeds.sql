c9318853f93e8324d0799e80a63fb275f8d87832
Jingsi <gongjingsi@hotmail.com>
2022-11-16 14:31:42 -0500

    add role table

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,3 +4,10 @@ VALUES
     ('Engineering'), 
     ('Finance'), 
     ('Legal');
+
+INSERT INTO role (title, salary, department_id)
+VALUES 
+    ('Sales Lead', 10000, 3), 
+    ('Salesperson', 8000, 3), 
+    ('Legal Team Lead', 25000, 4), 
+    ('Software Engineer', 15000, 2);
