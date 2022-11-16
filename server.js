4ff1ae34f8313d2fd199b987ee83a7eea15049bb
Jingsi <gongjingsi@hotmail.com>
2022-11-16 13:56:32 -0500

    set query for viewing, adding, and deleting department

--- server.js
+++ server.js
@@ -21,8 +21,29 @@ const db = mysql.createConnection(
     console.log('Connected to the employeetracker database.')
   );
 
+// Get all departments
 db.query(`SELECT * FROM department`, (err, rows) => {
 console.log(rows);
+});
+
+// Delete a department
+db.query(`DELETE FROM department WHERE id = ?`, 1, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(result);
+  });
+
+// Create a department
+const sql = `INSERT INTO department (name) 
+              VALUES (?)`;
+const params = ['Jane Test'];
+
+db.query(sql, params, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
 });
 
 app.get('/', (req, res) => {
