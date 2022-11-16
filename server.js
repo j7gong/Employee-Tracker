ff83dbfaf96b569c1afbba37493cd126e8ad3589
Jingsi <gongjingsi@hotmail.com>
2022-11-16 13:50:52 -0500

    set up connection to mysql database

--- server.js
+++ server.js
@@ -2,10 +2,28 @@ const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to MySQL database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'password',
+      database: 'employeetracker'
+    },
+    console.log('Connected to the employeetracker database.')
+  );
+
+db.query(`SELECT * FROM department`, (err, rows) => {
+console.log(rows);
+});
 
 app.get('/', (req, res) => {
     res.json({
